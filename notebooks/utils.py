ef7269f07cbcadfbcb18a4c749a087bedc78a7cb
Alejandro Benito-Santos <abenito@usal.es>
2023-03-29 10:59:49 +0200

    Updated data processing notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -13,22 +13,15 @@ def print_units_and_annotators(df):
 
 def calculate_krippendorff_alpha(df):
     """Calculate Krippendorff's alpha for the given annotations dataframe"""
-    def convert_to_ratings(df):
-        unit_n = len(df['pair_id'].unique())
-        annotator_n = len(df['annotator_id'].unique())
-        ratings = np.full((annotator_n, unit_n), np.nan)
-        for i, row in df.iterrows():
-            ratings[row['annotator_idx'], row['unit_idx']] = row['sim_rating']
-        return ratings
+    # Pivot the DataFrame to create a table where rows represent text pairs and columns represent annotators
+    pivot_df = df.pivot_table(index="pair_id", columns="annotator_id", values="sim_rating", aggfunc="first")
 
-    annotations_df = df.copy()
+    # # Replace missing values with NaN
+    alpha_df = pivot_df.replace({None: float("nan")})
 
-    annotator2idx = {annotator: idx for idx, annotator in enumerate(annotations_df['annotator_id'].unique())}
-    annotations_df['annotator_idx'] = annotations_df['annotator_id'].map(annotator2idx)
+    # Calculate Krippendorff's alpha
+    alpha = krippendorff.alpha(alpha_df.transpose(), level_of_measurement='ordinal')
+    
+    return alpha
 
-    unit2idx = {unit: idx for idx, unit in enumerate(annotations_df['pair_id'].unique())}
-    annotations_df['unit_idx'] = annotations_df['pair_id'].map(unit2idx)
-        
-    ratings = convert_to_ratings(annotations_df)
-    print(krippendorff.alpha(ratings, level_of_measurement='ordinal'))
-    return annotations_df, ratings
+
